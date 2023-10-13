19fb3928e3d51fb622fdb80587bea55a42af4caa
Joe Lai <joelai0101@gmail.com>
2023-10-13 10:17:18 +0800

    fix interval of ARB

--- main.cpp
+++ main.cpp
@@ -12,6 +12,7 @@ int main(int argc, const char * argv[]) {
     const double dirtyRate = 0.5;
     const double subsetRateA = 1.0 / 30.0;
     const double subsetRateB = 1.0 / 20.0;
+    const int interval = 100;
     int referenceRange = 20;
     double setSize = 20;
     double lambda = 1.0 / referenceSize; // 期望平均值與參考字串的大小 (Ref. size) 相關
@@ -59,7 +60,7 @@ int main(int argc, const char * argv[]) {
             performance = pageReplacement.Optimal();
             performance.printReport();
 
-            performance = pageReplacement.ARB(setSize);
+            performance = pageReplacement.ARB(interval);
             performance.printReport();
 
         }
